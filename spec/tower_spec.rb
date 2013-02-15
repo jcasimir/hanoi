f40204795c5f2583485777ed2721cfd3d0cd105f
Jeff Casimir <jeff@casimircreative.com>
2013-02-15 08:09:40 -0700

    Removing some repetition from tower_spec

--- spec/tower_spec.rb
+++ spec/tower_spec.rb
@@ -2,13 +2,17 @@ require 'spec_helper'
 
 describe Tower do 
   let(:tower){ Tower.new("a") }
+  let(:one){   Disc.new(1)    }
+  let(:two){   Disc.new(2)    }
+  let(:three){ Disc.new(3)    }
+  let(:four){  Disc.new(4)    }
 
   it "has a name" do
     expect(tower.name).to eq "a"
   end
 
   it "has discs" do
-    discs = [Disc.new(1), Disc.new(2), Disc.new(3), Disc.new(4)]
+    discs = [one, two, three, Disc.new(4)]
     tower.discs = discs
     expect(tower.discs).to eq discs
   end
@@ -19,53 +23,53 @@ describe Tower do
 
   context "#can_support?" do
     it "can support a smaller disc" do
-      tower.discs = [Disc.new(2)]
-      newbie = Disc.new(1)
+      tower.discs = [two]
+      newbie = one
       expect(tower.can_support?(newbie)).to be
     end
 
     it "cannot support a larger disc" do
-      tower.discs = [Disc.new(2)]
-      newbie = Disc.new(3)
+      tower.discs = [two]
+      newbie = three
       expect(tower.can_support?(newbie)).to_not be
     end
   end
 
   context "#top" do
     it "returns the top disc" do
-      tower.discs = [Disc.new(1), Disc.new(2)]
+      tower.discs = [one, two]
       expect(tower.top.size).to eq 1
     end
   end
 
   context "#stack" do
     it "adds a disc to the top" do
-      tower.discs = [Disc.new(2)]
-      tower.stack(Disc.new(1))
+      tower.discs = [two]
+      tower.stack(one)
       expect(tower.top.size).to eq 1
     end
 
     it "checks for stack validity" do
-      tower.discs = [Disc.new(3), Disc.new(1)]
-      expect{tower.stack(Disc.new(2))}.to raise_exception(InvalidStackError)
+      tower.discs = [three, one]
+      expect{tower.stack(two)}.to raise_exception(InvalidStackError)
     end
   end
 
   context "#take" do
     it "removes the top disc" do
-      tower.discs = [Disc.new(1), Disc.new(2)]
+      tower.discs = [one, two]
       expect(tower.take.size).to eq 1
     end
   end
 
   context "#validate!" do
     it "is cool with a valid stack" do
-      tower.discs = [Disc.new(1), Disc.new(2)]
+      tower.discs = [one, two]
       expect{tower.validate!}.to_not raise_exception(InvalidStackError)
     end
 
     it "is raises on an invalid stack" do
-      tower.discs = [Disc.new(2), Disc.new(1)]
+      tower.discs = [two, one]
       expect{tower.validate!}.to raise_exception(InvalidStackError)
     end
 
@@ -73,19 +77,19 @@ describe Tower do
 
   context "#valid?" do
     it "is cool with a valid stack" do
-      tower.discs = [Disc.new(1), Disc.new(2)]
+      tower.discs = [one, two]
       expect(tower.valid?).to be
     end
 
     it "is angry about an invalid stack" do
-      tower.discs = [Disc.new(2), Disc.new(1)]
+      tower.discs = [two, one]
       expect(tower.valid?).to_not be
     end
   end
 
   context "#size" do
     it "returns the number of discs when it has any" do
-      tower.discs = [Disc.new(1)]
+      tower.discs = [one]
       expect(tower.size).to eq 1
     end
 
